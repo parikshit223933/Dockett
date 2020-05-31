469b1499d51665c891ab30dc4c76d21e89c8d25d
parikshit singh <pk223933@gmail.com>
2020-05-31 06:14:47 +0530

    written all middlewares

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const port=8000;
+const app=express();
+
+app.set('case sensitive routing', false);
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({'extended': true}));
+app.use(express.static('assets'));
+app.get('/', function(req, res)
+{
+    return res.end('<h1>Hello mamasita</h1>');
+});
+
+
+
+
+app.listen(port, (error)=>
+{
+    if(error)
+    {
+        console.log('There was an error in starting the server');
+    }
+    else
+    {
+        console.log(`Server is running at port ${port}`);
+    }
+});
