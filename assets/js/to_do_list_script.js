93fbce9e624a993fda18b3230edeb2f95630f3b1
parikshit singh <pk223933@gmail.com>
2020-06-01 20:54:33 +0530

    Added comments

--- assets/js/to_do_list_script.js
+++ assets/js/to_do_list_script.js
@@ -1,12 +1,17 @@
 /* link handler function */
+/* no jquery is used in this javascript file. */
 function change_link(ids)
 {
+    /* if no task is selected for deletion, then the user will be prompted to select atleast one task to delete*/
+    /* but still the task will be added. */
     if(ids.length==0)
     {
         window.alert('Please select atleast one task to delete!');
     }
+    /* this is the very important portion of the js file. it will replace the value of href attribute with suitable query params in a single string */
     s="/delete-tasks/?"
     let count=0;
+    /* making the end point. in a form of "/delete-tasks/?id0=...&id1=...&..." */
     for(let id of ids)
     {
         s+="id"+count+"="
@@ -17,8 +22,10 @@ function change_link(ids)
         }
         count++;
     }
+    /* replacing the href of the delete button/link to the desired query params */
     document.querySelector('#task-action>a').setAttribute('href', s);
 }
+/* adding event listener to the add button, so that the action href changes to a desired end point. */
 document.querySelector('#task-action>a').addEventListener('click', function()
 {
     
@@ -42,6 +49,7 @@ for (let date of document.querySelectorAll('.text-muted'))
 }
 
 /* logo handler */
+/* if no category is choosen for a particular task, then all the category logos will be hidden from the user in the category list */
 for (let logo of document.querySelectorAll('.logo'))
 {
     if(logo.innerText=='Choose a Category')
@@ -52,7 +60,7 @@ for (let logo of document.querySelectorAll('.logo'))
 
 
 /* add button manager */
-
+/* if no field is filled for the task addition, then a warning will be shown to the user to select all the fields. but still all the tasks will be shown to him/her. */
 document.querySelector('button[type="submit"]').addEventListener('click', function()
 {
     if(document.querySelector('textarea').value==""||document.querySelector('#two-dropdowns select').value=="Choose a Category"||document.querySelector('#two-dropdowns input').value=="")
--- models/task.js
+++ models/task.js
@@ -1,4 +1,6 @@
+/* this is the mongoose module */
 const mongoose=require('mongoose');
+/* creatig our schema, The date can also be shown in the form of string, but since we need the names of the months, it is better to show it this way */
 const task_schema=new mongoose.Schema(
     {
         Description:{
@@ -13,6 +15,7 @@ const task_schema=new mongoose.Schema(
 
     }
 );
-
+/* creating our model with "task_schema" as Schema */
 const tasks=mongoose.model('tasks', task_schema);
+/* exporting our model so that it can be used by index.js */
 module.exports=tasks;
