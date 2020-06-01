1796540e23938de5368ab995ff265200a6d26514
parikshit singh <pk223933@gmail.com>
2020-06-01 18:58:52 +0530

    added alerts and handled bottlenecks

--- assets/js/to_do_list_script.js
+++ assets/js/to_do_list_script.js
@@ -1,5 +1,10 @@
+/* link handler function */
 function change_link(ids)
 {
+    if(ids.length==0)
+    {
+        window.alert('Please select atleast one task to delete!');
+    }
     s="/delete-tasks/?"
     let count=0;
     for(let id of ids)
@@ -16,6 +21,7 @@ function change_link(ids)
 }
 document.querySelector('#task-action>a').addEventListener('click', function()
 {
+    
     let ids=new Array();
     for(let checkbox of document.querySelectorAll('input[type="checkbox"]'))
     {
@@ -25,4 +31,32 @@ document.querySelector('#task-action>a').addEventListener('click', function()
         }
     }
     change_link(ids);
+})
+
+
+/* date handler function */
+
+for (let date of document.querySelectorAll('.text-muted'))
+{
+    date.innerText=date.innerText.toString().substring(0, 15);
+}
+
+/* logo handler */
+for (let logo of document.querySelectorAll('.logo'))
+{
+    if(logo.innerText=='Choose a Category')
+    {
+        logo.style.display="none"
+    }
+}
+
+
+/* add button manager */
+
+document.querySelector('button[type="submit"]').addEventListener('click', function()
+{
+    if(document.querySelector('textarea').value==""||document.querySelector('#two-dropdowns select').value=="Choose a Category"||document.querySelector('#two-dropdowns input').value=="")
+    {
+        window.alert('Remember to fill all the fields from next time.\nAn empty task will be added for you!');
+    }
 })
